2b6118d3e5430abd168223da5175a945ae90db2e
bertintshisuaka2 <bertintshisuaka@hotmail.com>
2023-02-09 00:54:32 -0500

    deployed

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const routes =require("./controllers");
+const app = express();
+const PORT = process.env.PORT||3001;
+app.use(express.static("public"));
+app.use(routes);
+app.listen(PORT,()=>{
+    console.log("I am a Software Developper!");
+});
